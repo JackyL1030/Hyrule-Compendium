cf66bce574e46c88846e39413bf3f13c23b7b75b
Jacky <jackylai1030@gmail.com>
2026-06-10 09:59:39 -0400

    add search functionality

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 import { getCategory } from "./modules/api.js";
 import { renderCards, openModal, closeModal } from "./modules/ui.js";
 
+let allMonsters = [];
+
 async function loadData() {
   try {
-    const monsters = await getCategory("monsters");
-    console.log(monsters[0]);
-    renderCards(monsters, openModal);
+    allMonsters = await getCategory("monsters");
+    console.log(allMonsters[0]);
+    renderCards(allMonsters, openModal);
 
     document
       .getElementById("close-modal")
       .addEventListener("click", closeModal);
+
+    setupSearch();
   } catch (error) {
     console.error(error);
   }
+}
+
+function setupSearch() {
+  const searchInput = document.getElementById("search-input");
+
+  searchInput.addEventListener("input", event => {
+    const searchTerm = event.target.value.toLowerCase();
+
+    const filteredMonsters = allMonsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchTerm)
+    );
+
+    renderCards(filteredMonsters, openModal);
+  });
 }
 
 loadData();
