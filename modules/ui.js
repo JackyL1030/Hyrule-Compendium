34c6d5c03a53851c12b459d49d2d8e02edc6a7a2
Jacky <jackylai1030@gmail.com>
2026-06-10 08:57:18 -0400

    rendered monster cards

---
+++ modules/ui.js
@@ -0,0 +1,14 @@
+export function renderCards(items) {
+  const container = document.getElementById("card-container");
+  container.innerHTML = "";
+
+  items.forEach((item) => {
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+    <img src="${item.image}" alt="${item.name}">
+    <h3>${item.name}</h3>
+    `;
+    container.appendChild(card);
+  });
+}
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import { getCategory } from "./modules/api.js";
+import { renderCards } from "./modules/ui.js";
 
 async function loadData() {
   try {
     const monsters = await getCategory("monsters");
 
-    console.log(monsters);
-    console.log(monsters[0]);
+    renderCards(monsters);
   } catch (error) {
     console.error(error);
   }
